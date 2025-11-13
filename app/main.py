eb96c6698d6c1da6937a29d5a75753f8e28607ee
sajeena000 <sajeenamalla000@gmail.com>
2025-11-13 10:50:14 +0545

    feat: Configure FastAPI app with routers and startup logic

---
+++ app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.config import Settings, get_settings
+from app.models.database import Base, engine
+from app.api.upload import router as upload_router
+from app.api.chat import router as chat_router
+
+app = FastAPI(
+    title="RAG API",
+    description="API for document ingestion and conversational RAG.",
+    version="1.0.0",
+)
+
+# CORS if needed
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.on_event("startup") #TODO: FIX THIS DEPRICATED WARNING
+def on_startup() -> None:
+    
+    Base.metadata.create_all(bind=engine)
+
+    
+    _ = get_settings() # Initialize settings on startup
+
+app.include_router(upload_router, prefix="/api")
+app.include_router(chat_router, prefix="/api")
